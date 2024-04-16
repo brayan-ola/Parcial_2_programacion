023423f1e7a156aff7eff3f677a6f79687606fdb
Bryan Giron <70452911+brayan-ola@users.noreply.github.com>
2024-04-16 08:19:19 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/hospital', function () {
     return view('hospital');
 })->name('hospital');
